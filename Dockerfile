6b38eb8aea2d9b902dc8a9a5b5d269655b8178a4
atsumarukun <atys.smz@gmail.com>
2023-07-07 15:23:50 +0900

    Create network.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.11
 
 RUN apt update && apt -y install portaudio19-dev && \
-    pip install numpy matplotlib pyaudio
+    pip install numpy matplotlib pyaudio tensorflow keras
--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-from utils.files import create_data
-from utils.files import read_file
-from utils.output import show_data
-from utils.output import playback_data
-from utils.devices import get_sound_devices
-from config import CORPUS_PATH, DATA_PATH
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras.layers import Dense, InputLayer
+
+from utils.files import read_files
+from config import DATA_PATH
 
 def main():
-    create_data(CORPUS_PATH, DATA_PATH)
-    playback_data(read_file("corpus/Natural/wav/01_MAD_1.wav"))
+    labels = ["JOY", "ACC", "FEA", "SUR", "SAD", "DIS", "ANG", "ANT", "NEU", "OTH"]
+    files = read_files(DATA_PATH)
+    random.shuffle(files)
+    nomalize = lambda x: (x - min(x)) / (max(x)  - min(x))
+    x_train = np.array([nomalize(f["data"]) for f in files[:int(len(files) * 0.9)]], "int16")
+    t_train = to_categorical(np.array([labels.index(f["path"][-8:-5]) for f in files[:int(len(files) * 0.9)]]), len(labels))
+    x_test = np.array([nomalize(f["data"]) for f in files[int(len(files) * 0.9):]], "int16")
+    t_test = to_categorical(np.array([labels.index(f["path"][-8:-5]) for f in files[int(len(files) * 0.9):]]), len(labels))
+
+    model = Sequential()
+    model.add(InputLayer(input_shape=(x_train.shape[1],)))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(len(labels), activation='relu'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.summary()
+
+    history = model.fit(x_train, t_train, epochs=10, batch_size=8)
+
+    plt.plot(np.arange(len(history.history["loss"])), history.history["loss"], label="loss")
+    plt.plot(np.arange(len(history.history["accuracy"])), history.history["accuracy"], label="acc")
+    plt.legend()
+    plt.show()
+
+    loss, acc = model.evaluate(x_test, t_test)
+    print(f"loss: {loss}, acc: {acc}")
 
 if __name__ == "__main__":
     main()
